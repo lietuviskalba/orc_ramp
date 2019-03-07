c1d3beac2220fb10cf9d1bbb1e985c63300cabf3
LietuvisKalba <mantasling@gmail.com>
2019-03-07 16:43:37 +0100

    Player movement done

---
+++ Scripts/CameraControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public GameObject player;
+    [Range(-5f, 50f)]
+    public float camHeight;
+    [Range(-55f, 50f)]
+    public float camDepth;
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;
+        playerPos.z += camDepth;
+        playerPos.y += camHeight;
+        transform.position = playerPos;
+    }
+}
---
+++ Scripts/Character.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    [Range(-10f,15f)]
+    public float speed = 5f;
+    [Range(0f, 15f)]
+    public float rotSpeed;
+
+    //god mode
+    public bool isMoving = true;
+
+    void Update()
+    {
+        RotateDir();
+        MoveForward();
+    }
+
+    private void RotateDir()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0f,-1f * rotSpeed, 0f, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0f, 1f * rotSpeed, 0f, Space.Self);
+        }
+    }
+
+    public void MoveForward()
+    {
+        if (isMoving == true)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision otherOb)
+    {
+        if (otherOb.gameObject.tag.Equals("Wall"))
+        {
+            transform.rotation = Quaternion.AngleAxis(180, transform.up) * transform.rotation;
+
+
+        }
+    }
+}
